d7482e2ee0e2f4935c87b8f21eae5973a84cd1bc
innerstella <suji0509@naver.com>
2024-07-17 14:16:30 +0900

    feat: rapvi 소개 en

--- src/locales/en/home.js
+++ src/locales/en/home.js
@@ -1,9 +1,47 @@
 export default {
   header: {
-    basicInfo: "Basic Info",
-    docs: "Docs",
+    basicInfo: "Features",
+    docs: "Resources",
   },
   basicInfo: {
-    menuTitle: "Basic Info",
+    menuTitle: "Features",
+    menu1: {
+      title: "Introduction",
+      text1:
+        "Hazardous vapor transport from soil and groundwater sources into indoor spaces contaminates indoor air and threatens occupant health. RAVPI (Risk Assessment Program for Vapor Intrusion) is a risk calculation web application (https://rapvi-ku.web.app/) based on conceptual site models designed to assess the vapor intrusion risks. RAPVI was developed by the Environmental Hydrogeology Laboratory of the Department of Earth and Environmental Sciences at Korea University with the support of the Korea Environmental Industry & Technology Institute.",
+      text2:
+        "You can find the RAPVI manual, user guide, and pre-processing documents via the link below. These materials are also available in the Resources tab along with the tutorial.",
+      text3: "Documents related to the RAPVI program",
+      text4:
+        "Comparison of RAPVI's Vapor Intrusion Risk Assessment MethodsComparison of RAPVI's Vapor Intrusion Risk Assessment Methods",
+      text5: "Basic assessment",
+      text6:
+        "This is a simple risk assessment model based on investigated data. Contaminant source, exposure pathway, and receptor information are input as numeric values and cancer/non-cancer risk risk graphs are produced.",
+      text7: "Single point",
+      text8: "Single/multiple contaminants",
+      text9: "Single layer",
+      text10: "Number input",
+      text11: "Number/graph output",
+      text12: "Site-specific assessment",
+      text13:
+        "This is an in-depth site-specific risk assessment model based on spatially distributed data. It receives contaminant source, exposure pathway, and receptor information as numeric values ​​and grids, and indoors contaminant concentrations, and cancer/non-cancer risk maps are produced.",
+      text14: "Multi-point",
+      text15: "Single contaminant",
+      text16: "Single/multi-layer",
+      text17: "Numeric value/grid input",
+      text18: "Map output",
+    },
+    menu2: {
+      title: "What is a vapor intrusion risk assessment",
+      text1: "위해성 평가란?",
+      text2:
+        "위해성 평가(Risk Assessment)란 유해성이 있는 화학물질이 사람과 환경에 노출되는 경우 사람의 건강이나 환경에 미치는 결과를 예측하기 위해 체계적으로 검토하고 평가하는 것을 의미합니다. 위해성 평가는 ‘1. 유해성 확인’, ‘2. 용량 반응평가’, ‘3. 노출평가’, ‘4. 위해도 결정’의 총 4단계로 구성되어 있습니다. 이 중 ‘3. 노출평가’와 ‘4. 위해도 결정’에서는 노출 시나리오에 기반 한 실제 농도 및 위해성 계산이 요구됩니다.",
+      text3: "증기침입이란?",
+      text4:
+        " 증기침입(Vapor Intrusion)이란 인체에 유해한 증기가 오염된 토양, 지하수와 같은 오염원으로부터 불포화대를 통과한 뒤, 건물 벽면 및 바닥의 틈, 지하수 사용 목적으로 연결한 도관 등을 통해 최종적으로 실내 공간으로 유입되는 이동 과정입니다. 증기침입은 도심 환경에서 위해성 평가의 대상이 되는 주요 경로입니다.",
+    },
+    menu3: {
+      title: "R&D team",
+    },
   },
 };
