c5c72734e8e634cf54ddca1c0d27592c187148f8
innerstella <suji0509@naver.com>
2024-07-20 23:40:01 +0900

    feat: 프로젝트 기본 정보 en

---
+++ src/locales/en/start.js
@@ -0,0 +1,33 @@
+export default {
+  projectInfo: {
+    title: "Project information",
+    section1: "Project title",
+    section2: "Project manager",
+    section3: "Project date/time",
+    section4: "Risk assessment type",
+    text: "What are basic assessment and site-specific assessment?",
+    button1: "Basic assessment",
+    button2: "Site-specific assessment",
+    map: {
+      section1: "Grid size (row X column)",
+      section2: "Coordinate1 (Latitude, Altitude)",
+      section3: "Coordinate2 (Latitude, Altitude)",
+      section4: "Coordinate3 (Latitude, Altitude)",
+      section5: "Coordinate4 (Latitude, Altitude)",
+      text: " ※ Please enter the coordinates of four points in the area corresponding to the study scope. The picture below is an example.",
+    },
+    nextButton: "Next",
+  },
+  typeInfo: {
+    title: "Risk assessment type selection",
+    section1: {
+      title: "Basic assessment",
+      text: "A simple risk assessment model based on existing data, calculating the risks by inputting the contaminant source, pathway, and receptor information of single/complex contaminants in numeric values format.",
+    },
+    section2: {
+      title: "Site-specific assessment",
+      text: "An in-depth, realistic risk assessment model based on spatial characteristic data, calculating a risk grid by receiving information on the source, pathway, and receptor of a single contaminant in a grid format.",
+    },
+    button: "Back",
+  },
+};
